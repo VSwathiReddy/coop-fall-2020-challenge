4c344671450cac73e24a1504887a42d12ded0782
VSwathiReddy <61486568+VSwathiReddy@users.noreply.github.com>
2020-07-10 14:37:44 -0400

    Update solution_python.py

--- solution_python.py
+++ solution_python.py
@@ -1,23 +1,37 @@
 class EventSourcer():
     # Do not change the signature of any functions
 
-    def __init__(self):
+    def _init_(self):
         self.value = 0
-
+        self.list1 = [0]
+        self.redo_item = 0
+        
     def add(self, num: int):
-        pass
+        self.num = num
+        self.value+=self.num
+        self.list1.append(self.value)
+        return self.value
 
     def subtract(self, num: int):
-        pass
+        self.num = num
+        self.value-=self.num
+        self.list1.append(self.value)
+        return self.value
 
     def undo(self):
-        pass
+        self.redo_item = self.list1.pop()
+        self.value = self.list1[-1]
+        return self.value
 
     def redo(self):
-        pass
+        self.value = self.redo_item
+        return self.value
 
     def bulk_undo(self, steps: int):
-        pass
+        for i in range(0,steps):
+            self.redo_item = self.list1.pop()
+        self.value = self.list1[-1]
+        return self.value
 
     def bulk_redo(self, steps: int):
         pass
